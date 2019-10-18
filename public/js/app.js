e2aed7a272f2997e48cb0f2626b5e950d9fba444
aionius <aionius@gmail.com>
2019-10-18 11:41:06 -0400

    added code for heroku compatibility

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,3 @@
-fetch("http://localhost:3000/weather?location=boston").then(response => {
-   response.json().then(data => {
-      console.log(data);
-   });
-});
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const forecast = document.querySelector("#forecast");
@@ -12,11 +6,9 @@ weatherForm.addEventListener("submit", event => {
    event.preventDefault();
    const location = search.value;
 
-   fetch("http://localhost:3000/weather?location=" + location).then(
-      response => {
-         response.json().then(data => {
-            forecast.textContent = data.forecastSummary;
-         });
-      }
-   );
+   fetch("/weather?location=" + location).then(response => {
+      response.json().then(data => {
+         forecast.textContent = data.forecastSummary;
+      });
+   });
 });
--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const hbs = require("hbs");
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 const publicDirPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
@@ -90,6 +92,6 @@ app.get("*", (req, res) => {
    });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
    console.log("Server is up on port 3000.");
 });
